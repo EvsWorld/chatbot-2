216d7a2c6cecf46ab6df6e8ce979dfda6e3930cc
evsworld <evanlhendrix@gmail.com>
2021-03-14 18:15:20 +0100

    imp: seed replies script

---
+++ services/replies/replies.service.js
@@ -0,0 +1,33 @@
+import 'dotenv/config';
+import fs from 'fs';
+import { Reply } from '../../models/reply.model';
+import { connectDb } from '../../db';
+
+const repliesToSeed = JSON.parse(
+  fs.readFileSync(__dirname + '/replies.json', 'utf-8')
+);
+
+/**
+ * BulkUpserts replies checking on the name field to not add replies already in db
+ *
+ **/
+export const bulkAdd = (arrayToBulkAdd) => {
+  console.log('arrayToBulkAdd :>> ', arrayToBulkAdd);
+  const bulkOps = arrayToBulkAdd.map((doc) => ({
+    updateOne: {
+      filter: { intent: doc.intent },
+      update: doc,
+      upsert: true,
+    },
+  }));
+
+  Reply.bulkWrite(bulkOps)
+    .then(function (characters) {})
+    .catch(function (err) {
+      console.error(err);
+    });
+};
+
+connectDb().then(async () => {
+   bulkAdd(repliesToSeed);
+});
